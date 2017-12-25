d2b59413c4c25f553252c75e9b5bfab532a3a36b
Matt Payne <Payne@MattPayne.org>
2017-12-25 03:26:41 +0000

    cucumber steps all pending...

---
+++ src/test/java/com/example/demo/CucumberStepDefinitions.java
@@ -0,0 +1,60 @@
+package com.example.demo;
+
+
+import cucumber.api.java.en.*;
+import cucumber.api.*;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class CucumberStepDefinitions {
+
+@When("^I create a user$")
+public void i_create_a_user() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Then("^I get a 'CREATED' response$")
+public void i_get_a_CREATED_response() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Given("^there exists a user$")
+public void there_exists_a_user() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@When("^I delete the user$")
+public void i_delete_the_user() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Then("^I get a 'OK' response$")
+public void i_get_a_OK_response() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@When("^I update the users's address$")
+public void i_update_the_users_s_address() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@When("^I update the address of a non-existent user$")
+public void i_update_the_address_of_a_non_existent_user() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Then("^I get a 'NOT_FOUND' response$")
+public void i_get_a_NOT_FOUND_response() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+}
+
